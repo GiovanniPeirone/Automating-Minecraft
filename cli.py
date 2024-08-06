bc1c0c8fbf2db581f5fd776f48411323da4ac1e2
Giovanni Peirone <mgpeirone@escuelasproa.edu.ar>
2024-08-06 20:40:11 -0300

    a

---
+++ cli.py
@@ -0,0 +1,26 @@
+import click
+import os 
+import math
+
+"""
+@click.group()
+def cli():
+    pass
+
+cli.command()
+def MovePlayer():
+    print("payer move")
+
+
+"""
+
+
+if __name__ == '__main__':
+    #cli()
+    playerPosX = 1
+    playerPosY = 1
+    playerPosZ = 1
+
+
+exit()
+
